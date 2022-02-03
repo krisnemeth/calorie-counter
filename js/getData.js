2290e87fa0204e77bda729fede53fd9b0e5afd72
Krisztian Nemeth <krsnmth@gmail.com>
2022-02-03 15:44:52 +0000

    finished counter functionality, everything works

--- js/getData.js
+++ js/getData.js
@@ -2,11 +2,14 @@ window.onload = function displayCal(){
     console.log('page loaded')
     let apple = Number(localStorage.getItem('apple'));
     let banana = Number(localStorage.getItem('banana'));
+    let blackberries = Number(localStorage.getItem('blackberries'));
+    let cherry = Number(localStorage.getItem('cherry'));
+    let kiwi = Number(localStorage.getItem('kiwi'));
+    let grapes = Number(localStorage.getItem('grapes'));
+    let orange = Number(localStorage.getItem('orange'));
+    let peach = Number(localStorage.getItem('peach'));
 
 
-    let fruit = 0;
-    fruit = 
-
-    
-    document.getElementById('output').innerHTML = `${apple}`;
+    document.getElementById('output').innerHTML = `${apple + banana + blackberries + cherry + kiwi + grapes + orange + peach}`;
 }
+
--- js/saveToLocal.js
+++ js/saveToLocal.js
@@ -12,3 +12,44 @@ function addB(){
     localStorage.setItem('banana', 65);
 }
 
+document.getElementById('addBlck').addEventListener('click', addBlck);
+
+function addBlck(){
+    console.log('btn clicked');
+    localStorage.setItem('blackberries', 25);
+}
+
+document.getElementById('addC').addEventListener('click', addC);
+
+function addC(){
+    console.log('btn clicked');
+    localStorage.setItem('cherry', 50);
+}
+
+document.getElementById('addK').addEventListener('click', addK);
+
+function addK(){
+    console.log('btn clicked');
+    localStorage.setItem('kiwi', 50);
+}
+
+document.getElementById('addG').addEventListener('click', addG);
+
+function addG(){
+    console.log('btn clicked');
+    localStorage.setItem('grapes', 62);
+}
+
+document.getElementById('addO').addEventListener('click', addO);
+
+function addO(){
+    console.log('btn clicked');
+    localStorage.setItem('orange', 30);
+}
+
+document.getElementById('addP').addEventListener('click', addP);
+
+function addP(){
+    console.log('btn clicked');
+    localStorage.setItem('peach', 30);
+}
